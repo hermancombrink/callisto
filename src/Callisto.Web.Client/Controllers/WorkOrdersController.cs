cacebc8d9da0d275c25b94c9654af61e9076eaeb
Herman Combrink <herman.combrink@gmail.com>
2018-03-17 09:53:43 +1300

    updated main theme page

---
+++ src/Callisto.Web.Client/Controllers/WorkOrdersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Callisto.Web.Client.Controllers
+{
+    public class WorkOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
