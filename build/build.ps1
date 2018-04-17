54efadecc60d18fdc6b15a3a089613d34e697bf4
Herman Combrink <herman.combrink@gmail.com>
2018-04-17 21:00:23 +1200

    bring up nginx and startup scripts

---
+++ build/build.ps1
@@ -0,0 +1,4 @@
+
+docker image build --tag callisto-landing --file docker\landing\dockerfile .
+
+docker image build --tag callisto-nginx --file docker\nginx\dockerfile .
---
+++ build/run.ps1
@@ -0,0 +1,3 @@
+docker container run -d -P --name callisto-landing callisto-landing
+
+docker container run -d -p 8090:80 -P --name callisto-nginx callisto-nginx
