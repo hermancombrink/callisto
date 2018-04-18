4114c6725a14f3401aa3bc0a3dfe33da00e0dd6b
Herman Combrink <herman.combrink@gmail.com>
2018-04-19 07:15:31 +1200

    updated docker build

--- build/build.ps1
+++ build/build.ps1
@@ -1,4 +1,46 @@
+Param
+(
+ [switch]$ignoreNpmRestore,
+ [switch]$dockerCompose
+)
 
-docker image build --tag callisto-landing --file docker\landing\dockerfile .
+$proj = (
+	"Callisto.Web.Landing"
+)
 
-docker image build --tag callisto-nginx --file docker\nginx\dockerfile .
+function getNpmInstallDir($project, $packageDir, $framework = "netcoreapp2.0"){
+	return ".\src\$project\bin\Release\$framework\publish\$packageDir";
+}
+
+Write-Host "dotnet clean..."
+dotnet clean -v q
+
+Write-Host "dotnet restore..."
+dotnet restore -nowarn:msb3202,nu1503 -v q
+
+foreach( $p in $proj)
+{
+	Write-Host "dotnet publish..."
+	dotnet publish ".\src\$p" -c Release --no-restore -v q
+
+	if($ignoreNpmRestore -eq $false)
+	{
+		Write-Host "npm install..."
+		$t = getNpmInstallDir $p -packageDir "wwwroot" 
+		npm --prefix $t install $t
+	}
+	else
+	{
+		Write-Host "Skipping npm install..."
+	}
+}
+
+if($dockerCompose)
+{
+	Write-Host "docker compose..."
+	docker-compose build
+}
+else
+	{
+		Write-Host "Skipping docker compose..."
+	}
--- build/run.ps1
+++ build/run.ps1
@@ -1,3 +1,1 @@
-docker container run -d -P --name callisto-landing callisto-landing
-
-docker container run -d -p 8090:80 -P --name callisto-nginx callisto-nginx
+docker-compose up -d
