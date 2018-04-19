6aef36caaf07030bbba1c53d4c368e5ff478f4c9
Herman Combrink <herman.combrink@gmail.com>
2018-04-19 21:29:11 +1200

    .

--- build/build.ps1
+++ build/build.ps1
@@ -1,6 +1,5 @@
 Param
 (
- [switch]$ignoreNpmRestore,
  [switch]$dockerCompose
 )
 
@@ -8,39 +7,39 @@ $proj = (
 	"Callisto.Web.Landing"
 )
 
-function getNpmInstallDir($project, $packageDir, $framework = "netcoreapp2.0"){
-	return ".\src\$project\bin\Release\$framework\publish\$packageDir";
+function getPublishDir($project, $framework = "netcoreapp2.0"){
+	return ".\src\$project\bin\Release\$framework";
 }
 
-Write-Host "dotnet clean..."
-dotnet clean -v q
-
-Write-Host "dotnet restore..."
-dotnet restore -nowarn:msb3202,nu1503 -v q
+Write-Host "dotnet restore..." -ForegroundColor Cyan
+dotnet restore -nowarn:msb3202,nu1503 -v q 
 
 foreach( $p in $proj)
 {
-	Write-Host "dotnet publish..."
+	Write-Host "building $p..." -ForegroundColor Green
+	Write-Host "dotnet publish..." -ForegroundColor Cyan
+	$t = getPublishDir $p
+	Remove-Item -Path $t -Force -Recurse
+
+	if(Test-Path -Path ".\src\$p\bower.json")
+	{
+		Write-Host "bower install..."  -ForegroundColor Cyan
+		pushd ".\src\$p"
+		bower install 
+		popd
+	}
+
 	dotnet publish ".\src\$p" -c Release --no-restore -v q
 
-	if($ignoreNpmRestore -eq $false)
-	{
-		Write-Host "npm install..."
-		$t = getNpmInstallDir $p -packageDir "wwwroot" 
-		npm --prefix $t install $t
-	}
-	else
-	{
-		Write-Host "Skipping npm install..."
-	}
+	
 }
 
 if($dockerCompose)
 {
-	Write-Host "docker compose..."
+	Write-Host "docker compose..."  -ForegroundColor Cyan
 	docker-compose build
 }
 else
 	{
-		Write-Host "Skipping docker compose..."
+		Write-Host "Skipping docker compose..."  -ForegroundColor Gray
 	}
