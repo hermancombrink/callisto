5313a9f829660bb7b8d062a094704e4d9403f0c5
Herman Combrink <herman.combrink@gmail.com>
2018-04-30 07:20:07 +1200

    moved gridoptions

--- src/Callisto.Web.App/ClientApp/src/app/home/home.component.ts
+++ src/Callisto.Web.App/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
 import { GridsterConfig, GridsterItem, GridType, DisplayGrid, CompactType } from 'angular-gridster2';
+import { GridOptions } from './GridOptions';
 
 @Component({
   selector: 'app-home',
@@ -10,63 +11,10 @@ import { GridsterConfig, GridsterItem, GridType, DisplayGrid, CompactType } from
 })
 export class HomeComponent implements OnInit {
 
-  options: GridsterConfig;
+  options: GridsterConfig = GridOptions.GridsterConfig;
   reports: Array<GridsterItem> = [];
 
   ngOnInit(): void {
-    this.options = {
-      gridType: GridType.Fit,
-      compactType: CompactType.None,
-      margin: 10,
-      outerMargin: true,
-      outerMarginTop: null,
-      outerMarginRight: null,
-      outerMarginBottom: null,
-      outerMarginLeft: null,
-      mobileBreakpoint: 640,
-      minCols: 1,
-      maxCols: 100,
-      minRows: 1,
-      maxRows: 100,
-      maxItemCols: 100,
-      minItemCols: 1,
-      maxItemRows: 100,
-      minItemRows: 1,
-      maxItemArea: 2500,
-      minItemArea: 1,
-      defaultItemCols: 1,
-      defaultItemRows: 1,
-      fixedColWidth: 105,
-      fixedRowHeight: 105,
-      keepFixedHeightInMobile: false,
-      keepFixedWidthInMobile: false,
-      scrollSensitivity: 10,
-      scrollSpeed: 20,
-      enableEmptyCellClick: false,
-      enableEmptyCellContextMenu: false,
-      enableEmptyCellDrop: false,
-      enableEmptyCellDrag: false,
-      emptyCellDragMaxCols: 50,
-      emptyCellDragMaxRows: 50,
-      ignoreMarginInRow: false,
-      draggable: {
-        enabled: true,
-      },
-      resizable: {
-        enabled: true,
-      },
-      swap: false,
-      pushItems: true,
-      disablePushOnDrag: false,
-      disablePushOnResize: false,
-      pushDirections: { north: true, east: true, south: true, west: true },
-      pushResizeItems: false,
-      displayGrid: DisplayGrid.Always,
-      disableWindowResize: false,
-      disableWarnings: false,
-      scrollToNewItems: false
-    };
-
     this.reports = [
       { cols: 2, rows: 1, y: 0, x: 0 },
       { cols: 2, rows: 2, y: 0, x: 2 },
