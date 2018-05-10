6bf22fdef7e2731aa1e2e07906009e00578a1e51
Herman Combrink <herman.combrink@gmail.com>
2018-05-11 06:54:42 +1200

    updated asset test coverage

--- test/Callisto.Tests/AssetsTests.cs
+++ test/Callisto.Tests/AssetsTests.cs
@@ -2,15 +2,25 @@ using AutoFixture;
 using Callisto.Module.Assets;
 using Callisto.Module.Assets.Interfaces;
 using Callisto.Module.Assets.Repository.Models;
+using Callisto.SharedKernel;
 using Callisto.SharedKernel.Enum;
 using Callisto.SharedModels.Assets.ViewModels;
+using Callisto.SharedModels.Auth;
+using Callisto.SharedModels.Auth.ViewModels;
+using Callisto.SharedModels.Location;
+using Callisto.SharedModels.Location.ViewModels;
 using Callisto.SharedModels.Session;
 using Callisto.SharedModels.Storage;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Logging;
+using Moq;
 using NSubstitute;
 using System;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -110,7 +120,7 @@ namespace Callisto.Tests
         /// </summary>
         /// <returns>The <see cref="Task"/></returns>
         [Fact]
-        public async Task GetAssetByShouldReturnViewModel()
+        public async Task GetAssetByIdShouldReturnWhenAllIswell()
         {
             var fixture = new Fixture();
             var repo = Substitute.For<IAssetsRepository>();
@@ -118,10 +128,34 @@ namespace Callisto.Tests
             var asset = fixture.Create<Asset>();
             repo.GetAssetById(Arg.Any<Guid>()).Returns(asset);
             var assetModule = new AssetsModule(session, Substitute.For<ILogger<AssetsModule>>(), repo, Storage);
-            var model = fixture.Create<AssetAddViewModel>();
             var result = await assetModule.GetAssetAsync(asset.Id);
             result.Status.Should().Be(RequestStatus.Success);
             repo.Received(1).GetAssetById(Arg.Any<Guid>());
+        }
+
+        /// <summary>
+        /// The GetAssetByShouldReturnViewModel
+        /// </summary>
+        /// <returns>The <see cref="Task"/></returns>
+        [Fact]
+        public async Task GetAssetDetailByIdShouldReturnWhenAllIswell()
+        {
+            var fixture = new Fixture();
+            var repo = Substitute.For<IAssetsRepository>();
+            var location = Substitute.For<ILocationModule>();
+            var session = Substitute.For<ICallistoSession>();
+
+            location.GetLocation(Arg.Any<long>()).Returns(c => fixture.Create<RequestResult<LocationViewModel>>());
+            session.Location.Returns(c => location);
+            var asset = fixture.Create<Asset>();
+            repo.GetAssetById(Arg.Any<Guid>()).Returns(asset);
+            repo.GetAssetLocationByAssetId(Arg.Any<long>()).Returns(c => fixture.Create<AssetLocation>());
+            var assetModule = new AssetsModule(session, Substitute.For<ILogger<AssetsModule>>(), repo, Storage);
+            var result = await assetModule.GetAssetDetailsAsync(asset.Id);
+
+            result.Status.Should().Be(RequestStatus.Success);
+            repo.Received(1).GetAssetById(Arg.Any<Guid>());
+            location.Received(1).GetLocation(Arg.Any<long>());
         }
 
         /// <summary>
@@ -133,15 +167,43 @@ namespace Callisto.Tests
         {
             var fixture = new Fixture();
             var repo = Substitute.For<IAssetsRepository>();
+            var location = Substitute.For<ILocationModule>();
             var session = Substitute.For<ICallistoSession>();
+            location.UpsertLocation(Arg.Any<LocationViewModel>()).Returns(c => RequestResult<long>.Success(1));
+            session.Location.Returns(c => location);
             var viewModel = fixture.Create<AssetDetailViewModel>();
             repo.GetAssetById(Arg.Any<Guid>()).Returns(fixture.Create<Asset>());
+            repo.BeginTransaction().Returns(c => Substitute.For<IDbContextTransaction>());
+
             var assetModule = new AssetsModule(session, Substitute.For<ILogger<AssetsModule>>(), repo, Storage);
             var model = fixture.Create<AssetViewModel>();
             var result = await assetModule.SaveAssetAsync(viewModel);
             result.Status.Should().Be(RequestStatus.Success);
             repo.Received(1).GetAssetById(Arg.Any<Guid>());
             repo.Received(1).SaveAssetAsync(Arg.Any<Asset>());
+            repo.Received(1).AddAssetLocation(Arg.Any<AssetLocation>());
+        }
+
+        [Fact]
+        public void SaveAssetShouldFailWhenLocationFails()
+        {
+            var fixture = new Fixture();
+            var repo = Substitute.For<IAssetsRepository>();
+            var location = Substitute.For<ILocationModule>();
+            var session = Substitute.For<ICallistoSession>();
+            location.UpsertLocation(Arg.Any<LocationViewModel>()).Returns(c => RequestResult<long>.Failed("oops"));
+            session.Location.Returns(c => location);
+            var viewModel = fixture.Create<AssetDetailViewModel>();
+            repo.GetAssetById(Arg.Any<Guid>()).Returns(fixture.Create<Asset>());
+            repo.BeginTransaction().Returns(c => Substitute.For<IDbContextTransaction>());
+
+            var assetModule = new AssetsModule(session, Substitute.For<ILogger<AssetsModule>>(), repo, Storage);
+            var model = fixture.Create<AssetViewModel>();
+            Func<Task> act = async () => { var result = await assetModule.SaveAssetAsync(viewModel); };
+            act.Should().Throw<InvalidOperationException>();
+            repo.Received(1).GetAssetById(Arg.Any<Guid>());
+            repo.Received(1).SaveAssetAsync(Arg.Any<Asset>());
+            repo.Received(0).AddAssetLocation(Arg.Any<AssetLocation>());
         }
 
         /// <summary>
@@ -223,6 +285,73 @@ namespace Callisto.Tests
             act.Should().Throw<InvalidOperationException>();
             repo.Received(0).GetAssetTree(Arg.Any<long>(), Arg.Any<long>());
             repo.Received(1).GetAssetById(Arg.Any<Guid>());
+        }
+
+        [Fact]
+        public async Task UploadAssetPicShouldSucceedWhenAllIsWell()
+        {
+            var fixture = new Fixture();
+            var repo = Substitute.For<IAssetsRepository>();
+            var auth = Substitute.For<IAuthenticationModule>();
+            var session = Substitute.For<ICallistoSession>();
+            var storage = Substitute.For<IStorage>();
+            var file = Substitute.For<IFormFile>();
+            file.OpenReadStream().Returns(c => new MemoryStream(Encoding.UTF8.GetBytes("abc")));
+            storage.SaveFile(Arg.Any<byte[]>(), Arg.Any<string>(), Arg.Any<string>()).Returns(c => "https://test.com");
+            session.Authentication.Returns(c => auth);
+            auth.GetCompanyByRefId(Arg.Any<long>()).Returns(c => RequestResult<CompanyViewModel>.Success(fixture.Create<CompanyViewModel>()));
+
+            repo.GetAssetById(Arg.Any<Guid>()).Returns(fixture.Create<Asset>());
+            var assetModule = new AssetsModule(session, Substitute.For<ILogger<AssetsModule>>(), repo, storage);
+            
+            var result = await assetModule.UploadAssetPicAsync(file, Guid.NewGuid());
+            result.Status.Should().Be(RequestStatus.Success);
+            repo.Received(1).GetAssetById(Arg.Any<Guid>());
+            repo.Received(1).SaveAssetAsync(Arg.Any<Asset>());
+            result.Result.Should().Be("https://test.com");
+        }
+
+
+        [Fact]
+        public async Task UpdateParentShouldSucceedWhenAllIsWell()
+        {
+            var fixture = new Fixture();
+            var repo = Substitute.For<IAssetsRepository>();
+            var session = Substitute.For<ICallistoSession>();
+            repo.GetAssetById(Arg.Any<Guid>()).Returns(c => fixture.Create<Asset>());
+
+            var assetModule = new AssetsModule(session, Substitute.For<ILogger<AssetsModule>>(), repo, Storage);
+            var result = await assetModule.UpdateParentAsync(Guid.NewGuid(), Guid.NewGuid());
+            result.Status.Should().Be(RequestStatus.Success);
+            repo.Received(1).SaveAssetAsync(Arg.Any<Asset>());
+        }
+
+        [Fact]
+        public async Task UpdateParentShouldSucceedWhenParentIsNull()
+        {
+            var fixture = new Fixture();
+            var repo = Substitute.For<IAssetsRepository>();
+            var session = Substitute.For<ICallistoSession>();
+            repo.GetAssetById(Arg.Any<Guid>()).Returns(c => fixture.Create<Asset>());
+
+            var assetModule = new AssetsModule(session, Substitute.For<ILogger<AssetsModule>>(), repo, Storage);
+            var result = await assetModule.UpdateParentAsync(Guid.NewGuid());
+            result.Status.Should().Be(RequestStatus.Success);
+            repo.Received(1).SaveAssetAsync(Arg.Any<Asset>());
+        }
+
+        [Fact]
+        public async Task RemoveAssetShouldSucceedWhenAllIsWell()
+        {
+            var fixture = new Fixture();
+            var repo = Substitute.For<IAssetsRepository>();
+            var session = Substitute.For<ICallistoSession>();
+            repo.GetAssetById(Arg.Any<Guid>()).Returns(c => fixture.Create<Asset>());
+
+            var assetModule = new AssetsModule(session, Substitute.For<ILogger<AssetsModule>>(), repo, Storage);
+            var result = await assetModule.RemoveAssetAsync(Guid.NewGuid());
+            result.Status.Should().Be(RequestStatus.Success);
+            repo.Received(1).RemoveAssetAsync(Arg.Any<Asset>());
         }
     }
 }
