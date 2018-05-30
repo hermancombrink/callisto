904dc2af80563642363e6ff468fe90920c630e73
Herman Combrink <herman.combrink@gmail.com>
2018-05-30 19:47:08 +1200

    .

---
+++ src/Callisto.SharedModels/Person/IPersonRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Callisto.SharedModels.Person
+{
+    /// <summary>
+    /// Defines the <see cref="IPersonRepository" />
+    /// </summary>
+    public interface IPersonRepository<T>
+    {
+        /// <summary>
+        /// The AddPerson
+        /// </summary>
+        /// <param name="person">The <see cref="T"/></param>
+        /// <returns>The <see cref="Task"/></returns>
+        Task AddPerson(T person);
+
+        /// <summary>
+        /// The RemovePerson
+        /// </summary>
+        /// <param name="person">The <see cref="T"/></param>
+        /// <returns>The <see cref="Task"/></returns>
+        Task RemovePerson(T person);
+
+        /// <summary>
+        /// The UpdatePerson
+        /// </summary>
+        /// <param name="person">The <see cref="T"/></param>
+        /// <returns>The <see cref="Task"/></returns>
+        Task UpdatePerson(T person);
+
+        /// <summary>
+        /// The GetPerson
+        /// </summary>
+        /// <param name="refId">The <see cref="long"/></param>
+        /// <returns>The <see cref="Task{T}"/></returns>
+        Task<T> GetPerson(long refId);
+
+        /// <summary>
+        /// The GetPerson
+        /// </summary>
+        /// <param name="id">The <see cref="Guid"/></param>
+        /// <returns>The <see cref="Task{T}"/></returns>
+        Task<T> GetPerson(Guid id);
+
+        /// <summary>
+        /// The GetPeople
+        /// </summary>
+        /// <returns>The <see cref="Task{IEnumerable{T}}"/></returns>
+        Task<IEnumerable<T>> GetPeople();
+    }
+}
---
+++ src/Callisto.SharedModels/Person/ViewModel/PersonBaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Callisto.SharedModels.Person.ViewModel
+{
+    /// <summary>
+    /// Defines the <see cref="PersonBaseViewModel" />
+    /// </summary>
+    public abstract class PersonBaseViewModel
+    {
+        /// <summary>
+        /// Gets or sets the Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the FirstName
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the LastName
+        /// </summary>
+        public string LastName { get; set; }
+    }
+}
